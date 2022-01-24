4787a06839600a41b18d0e0f356267d241ba106c
Kristen <lloyd.kristen91@gmail.com>
2022-01-24 14:16:57 -0500

    clean up

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System;
 using System.IO;
 using System.Linq;
 using System.ComponentModel.DataAnnotations;
-//using Microsoft.VisualBasic.FileIO;
+
 // Text input for file name
 // Iterate thru directory of files to confirm file exists
 //use Directory class to search by extension?
@@ -27,48 +27,36 @@ namespace CSV
             //from documentation
             string currentDirectory = Directory.GetCurrentDirectory();
 
-            var files = Directory.GetFiles(currentDirectory, "*.csv");
+            //var files = Directory.GetFiles(currentDirectory, "*.csv");
 
-            // var files = from retrievedFile in Directory.EnumerateFiles(currentDirectory, "*.csv", SearchOption.AllDirectories)
 
-            //             from line in File.ReadLines(retrievedFile)
-            //             where line.Contains(inputFile)
-            //             select new
-            //             {
-            //                 File = retrievedFile,
-            //                 Line = line
-            //             };
-
-            //initialize lists for valid and invalid emails
-            List<string> validEmail = new List<string>();
-            List<string> invalidEmail = new List<string>();
-
-            //validEmail.Add() and invalidEmail.Add() to add to list
             if (File.Exists(inputFile))
             {
                 string fullPath = Path.GetFullPath(inputFile);
-                Console.WriteLine(fullPath);
+
                 Console.WriteLine("File {0} exists", inputFile);
-                //array with each line
+                //array with each line as a string
                 string[] csvLines = File.ReadAllLines(fullPath);
+                //initialize lists for valid and invalid emails
+                List<string> validEmail = new List<string>();
+                List<string> invalidEmail = new List<string>();
 
                 //list with email data
                 var emails = new List<string>();
 
                 foreach (var line in csvLines)
                 {
+                    //split the string at the comma
                     string[] rowData = line.Split(',');
                     emails.Add(rowData[2]);
                 }
 
-
                 //all emails added to a list
                 foreach (var email in emails)
                 {
-                    Console.WriteLine(email);
                     var checkEmail = new EmailAddressAttribute();
                     bool valid;
-                    valid = checkEmail.IsValid(email); //change this to email from csv file
+                    valid = checkEmail.IsValid(email);
 
                     if (valid == true)
                     {
@@ -80,19 +68,19 @@ namespace CSV
                         valid = false;
                         invalidEmail.Add(email);
                     }
+                    //valid is not catching example@mail b/c there are valid emails w/ that edgecase like example@localhost or user@localserver
+                }
+                Console.WriteLine("Valid Email List:");
+                foreach (string email in validEmail)
+                {
+                    Console.WriteLine(email);
                 }
 
-
-                // foreach (var f in emails)
-                // {
-                //     //parse csv
-                //     //if file exists iterate thru each row to conf email is accurate
-
-
-
-                //     //EmailAddress Attribute validates an email address
-
-                // }
+                Console.WriteLine("Invalid Email List:");
+                foreach (string email in invalidEmail)
+                {
+                    Console.WriteLine(email);
+                }
 
             }
             else if (!File.Exists(inputFile))
@@ -100,22 +88,6 @@ namespace CSV
                 Console.WriteLine("{0} does not exist in this Directory", inputFile);
             }
 
-
-
-
-
-            //print lists of valid and invalid email lists
-            Console.WriteLine("Valid Email List:");
-            foreach (string email in validEmail)
-            {
-                Console.WriteLine(email);
-            }
-
-            Console.WriteLine("Invalid Email List:");
-            foreach (string email in invalidEmail)
-            {
-                Console.WriteLine(email);
-            }
         }
 
     }
